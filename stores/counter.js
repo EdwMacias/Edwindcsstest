47aa35d364856e5f11ef36333444083016164240
Nuxt.Macias <edwardavid26@hotmail.com>
2023-11-23 21:49:11 -0500

    Se agrega composition API, se deja comentario de uso de options api, se diferencia en la declaracion de las variables, ademas se retornan los usos de las funciones, de lo contrario no se pueden accedes a los estados de pinia, es OBLIGATORIO return

--- stores/counter.js
+++ stores/counter.js
@@ -1,21 +1,36 @@
 import { defineStore} from "pinia";
 
-export const useCounterStore = defineStore('counter', {
-    state: () => ({
-        count: 0,
-        name: "Edwar",
-    }),
-    getters: {
-        doubleCount: (state) => state.count * 2,
-        nameUppercase: (state) => state.name.toUpperCase(),
-    },
-    actions: {
-        increment() {
-            this.count++
-        },
-        reverseName() {
-            const reverse = this.name.split('').reverse().join('')
-            this.name = reverse
-        }
-    },
+export const useCounterStore = defineStore( "counter", ()=>{
+    const count = ref(0)
+    const name = ref("Edwar")
+    const doubleCount = computed(() => count.value * 2)
+    const nameUppercase = computed(() => name.value.toUpperCase())
+    function increment(){
+        count.value++
+    }
+    function reverseName(){
+        const reverse = name.value.split('').reverse().join('')
+        name.value = reverse
+    }
+    return {count, name, increment, reverseName, doubleCount, nameUppercase}
 })
+
+// export const useCounterStore = defineStore('counter', {
+//     state: () => ({
+//         count: 0,
+//         name: "Edwar",
+//     }),
+//     getters: {
+//         doubleCount: (state) => state.count * 2,
+//         nameUppercase: (state) => state.name.toUpperCase(),
+//     },
+//     actions: {
+//         increment() {
+//             this.count++
+//         },
+//         reverseName() {
+//             const reverse = this.name.split('').reverse().join('')
+//             this.name = reverse
+//         }
+//     },
+// })
